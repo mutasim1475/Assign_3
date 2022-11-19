e6945f77020f9eebd7b4482471c2ada630a9777d
Mutasim Rahman <mutasim.rahman@ontariotechu.net>
2022-11-18 20:43:15 -0500

    Got rid of code to fix alert not showing

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,21 +1,21 @@
-const { event } = require("jquery");
-
 // IIFE --> Immediately invoked function expression
 (function(){
     function Start()
     {
-        console.log("App Started");
-        let deleteButtons = document.querySelectorAll('.btn-danger');
+        console.log("App Started")
+
+        let deleteButtons = document.querySelectorAll('.fa-delete2')
     for(button of deleteButtons)
     {
-        button.addEventListener('click', (event)=>{
-            if(!confirm("Are you sure?"))
+        button.addEventListener('click',(event)=>{
+            if(!confirm("Are you sure? "))
             {
-                event.preventDefault();
-                window.location.assign('/Standings');
+                event.preventDefault()
+                window.location.assign('/Standings')
             }
         });
+
     }
     }
-    window.addEventListener("load", Start);
-})();
+    window.addEventListener("load",Start)
+})()
